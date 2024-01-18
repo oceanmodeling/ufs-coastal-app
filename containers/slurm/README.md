4ba88ed48ea71011d8930f55107a115ba2a9f105
Ufuk Turuncoglu <turuncu@ucar.edu>
2024-01-18 05:25:04 +0000

    update slurm container

--- containers/slurm/README.md
+++ containers/slurm/README.md
@@ -1,2 +1,18 @@
-## Slurm Slurm Workload Manager Docker Container
+## Slurm Workload Manager Docker Container
 
+The Slurm container is a self consistent [Docker](https://www.docker.com) container that allows to use/practice Slurm commands under Ubuntu 22.04.
+
+To build and run the conatiner, docker commands need to be available. [Docker Desktop](https://www.docker.com/products/docker-desktop/) can be used for this purpose.
+
+The container can be built using following commands,
+
+```shell
+$ cd ufs-coastal-app/containers/slurm
+$ docker build -t slurm:latest .
+```
+
+Once the container is built, the following command can be use to run it.
+
+```shell
+$ docker run --platform linux/amd64 --hostname=linux -it slurm:latest bash
+```
--- containers/slurm/entrypoint.sh
+++ containers/slurm/entrypoint.sh
@@ -1,47 +1,58 @@
 #!/bin/bash
 
+# add host name
 setup_hosts() {
-  # add host name
   echo "127.0.0.1	$(hostname)" >> /etc/hosts
 }
 
+# configure mysql service
 setup_mysql() {
   # create home directory for mysql user
   usermod -d /var/lib/mysql/ mysql
+
   # fix permission of mysql folder
   chmod -R 755 /var/run/mysqld
+
   # start service
   service mysql start 
+
   # create database and set permissions
   mysql -u root -e " \
     create user 'slurm'@'$(hostname)' identified by 'password'; \
     grant all on slurm_acct_db.* TO 'slurm'@'$(hostname)'; \
+    create user 'slurm'@'localhost' identified by 'password'; \
+    grant all on slurm_acct_db.* TO 'slurm'@'localhost'; \
     create database slurm_acct_db; \
     flush privileges;"
 }
 
+# configure slurm
 slurm_config() {
-  # update configuration files for hostname, cpu and memory information
+  # edit configuration
   sed -i "s/<<HOSTNAME>>/$(hostname)/" /etc/slurm/slurm.conf
-  sed -i "s/<<CPU>>/$(nproc)/" /etc/slurm/slurm.conf
+  sed -i "s/<<CPU>>/$(cat /proc/cpuinfo | grep "processor" | wc -l)/" /etc/slurm/slurm.conf
   sed -i "s/<<MEMORY>>/$(if [[ "$(slurmd -C)" =~ RealMemory=([0-9]+) ]]; then echo "${BASH_REMATCH[1]}"; else exit 100; fi)/" /etc/slurm/slurm.conf
   sed -i "s/<<HOSTNAME>>/$(hostname)/" /etc/slurm/slurmdbd.conf
+
   # fix permissions
   chmod 600 /etc/slurm/slurmdbd.conf
   chown slurm:slurm /etc/slurm/slurmdbd.conf
   chmod 640 /etc/slurm/slurm.conf
   chown slurm:slurm /etc/slurm/slurm.conf
+
   # create directory for slurm controller
   mkdir -p /var/spool/slurmctld
   chown slurm:slurm /var/spool/slurmctld
 }
 
+# add slurm user
 slurm_add_user() {
   sacctmgr -Q -i add cluster mycluster
   sacctmgr -Q -i add account default description "Default account" Organization=Regular
   sacctmgr -Q -i create user name=ufs account=default
 }
 
+# restart all slurm related services
 slurm_all_restart() {
   service munge restart
   service slurmdbd restart
@@ -49,7 +60,7 @@ slurm_all_restart() {
   service slurmctld restart
 }
 
-# call functions with sudo rights
+# call functions as root
 setup_hosts
 setup_mysql
 slurm_config
